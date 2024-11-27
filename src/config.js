5154bd48ff78cc4ae75146f401e67e4474661663
Pietro Nunes <62605065+pietro-nunes@users.noreply.github.com>
2024-11-27 11:26:13 -0300

    commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  baseURL: "https://direct-delivery-api-d59eac383c33.herokuapp.com/", // Substitua pela URL do seu servidor
+  baseURL: "https://direct-delivery-api-d59eac383c33.herokuapp.com", // Substitua pela URL do seu servidor
 };
 
 export default config;
