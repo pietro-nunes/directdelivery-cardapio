48df4d3287b0b454818cfaf999c5c561e1b5ac72
Pietro Nunes <62605065+pietro-nunes@users.noreply.github.com>
2025-05-14 08:28:47 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // baseURL: "https://direct-delivery-api-d59eac383c33.herokuapp.com", // Substitua pela URL do seu servidor
-  baseURL: "http://localhost:3333"
+  baseURL: "https://direct-delivery-api-d59eac383c33.herokuapp.com", // Substitua pela URL do seu servidor
+  // baseURL: "http://localhost:3333"
 };
 
 export default config;
