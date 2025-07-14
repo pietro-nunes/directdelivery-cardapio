e9d8c27d2d7b151c31542d301ff8ea687914968d
Pietro Nunes <62605065+pietro-nunes@users.noreply.github.com>
2025-07-14 20:40:44 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  baseURL: "http://api.directdelivery.shop:9600", // Substitua pela URL do seu servidor
+  baseURL: "https://api.directdelivery.shop", // Substitua pela URL do seu servidor
   // baseURL: "http://localhost:3333"
 };
 
