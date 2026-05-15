f9fc242bdeb9adfb91e08e4d906fd6f14474b4a8
Pietro Nunes <62605065+pietro-nunes@users.noreply.github.com>
2026-05-15 16:23:08 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // baseURL: "https://api-homologacao.directdelivery.shop", // Substitua pela URL do seu servidor
-  baseURL: "http://localhost:3333"
+  baseURL: "https://api-homologacao.directdelivery.shop", // Substitua pela URL do seu servidor
+  // baseURL: "http://localhost:3333"
 };
 
 export default config;
